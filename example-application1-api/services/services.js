1a4345f041564aa25f81f039f657b18bc7443552
seifeddine <seifeddine.benkhaled@we-settle.com>
2022-10-10 04:09:34 +0100

    add services

---
+++ example-application1-api/services/services.js
@@ -0,0 +1,22 @@
+// import { User } from "../models/models";
+const models = require('../models/models');
+const randomId = require("random-id");
+
+// place holder for the data
+const users = [];
+
+function addUser(user) {
+  user.id = randomId(10);
+  // user.email = "a@a.com";
+  console.log("Adding user hhhhh", user);
+
+  models.User.create(user);
+
+  users.push(user);
+}
+
+function getUsers(user) {
+    return users;
+}
+
+module.exports = { addUser, getUsers };
