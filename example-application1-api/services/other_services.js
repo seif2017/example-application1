68707677d403c43d5b39149b408be203bfad05f8
seifeddine <seifeddine.benkhaled@we-settle.com>
2022-10-12 05:33:15 +0100

    enhance logs

--- example-application1-api/services/other_services.js
+++ example-application1-api/services/other_services.js
@@ -1,12 +1,13 @@
-const {logs} = require("../logging/logService"); 
+const { logs } = require("../logging/logService");
 
 exports.getDate = () => {
   // console.log(process.env.TZ)
   // process.env.TZ = 'Africa/Tunis';
   // console.log(process.env.TZ)
+  logs("[INFO]", "process.env.TZ:", process.env.TZ);
 
   const dt = new Date(); // ==> donne UTC   si process.env.TZ est vide
-  logs("[INFO]",dt);
+  logs("[INFO]", "Server time:", dt);
   const dat =
     dt.toLocaleDateString("fr", {
       month: "2-digit",
@@ -20,11 +21,16 @@ exports.getDate = () => {
       second: "2-digit",
     });
 
-  logs("[INFO]",dat);
+  logs(
+    "[INFO]",
+    "Server formatted (toLocaleDateString, toLocaleTimeString, fr) time:",
+    dat
+  );
   return dat;
 };
 
 exports.getEnv = () => {
   const env = process.env.XXX;
+  logs("[INFO]", "Server env XXX:", env);
   return env;
 };
