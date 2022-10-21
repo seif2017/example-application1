02b0e509849d96be658e852f258e25d94d8005d8
seifeddine <seifeddine.benkhaled@we-settle.com>
2022-10-21 23:45:21 +0100

    maj

--- example-application1-api/server.js
+++ example-application1-api/server.js
@@ -10,13 +10,7 @@ process.env.TZ = "Africa/Tunis";
 
 require("dotenv").config();
 
-const { logs } = require("./logging/logService");
 
-const {
-  logErrorMiddleware,
-  returnErrorMiddleware,
-} = require("./errors/errorHandler");
-const error = require("./errors/error");
 
 app.use(bodyParser.json());
 app.use(
@@ -30,14 +24,16 @@ app.use(function (req, res, next) {
   next();
 });
 
-// const db = require("./models");
-// db.sequelize.sync();
-// logs("[INFO]","DB SYNC invoked");
-// db.gouvernorats.create({ code: "1000", libelle: "TUNIS" }).catch(err => {});
-// db.gouvernorats.create({ code: "1100", libelle: "ARIANA" }).catch(err => {});
-
+const { logs } = require("./logging/logService");
+const error = require("./errors/error");
+const {
+  logErrorMiddleware,
+  returnErrorMiddleware,
+} = require("./errors/errorHandler");
 const logRequestMiddleware = require("./logging/logRequests");
 app.use(logRequestMiddleware);
+app.use(logErrorMiddleware);
+app.use(returnErrorMiddleware);
 
 app.get("/", (req, res) => {
   logs("[INFO]", "landing page");
@@ -48,15 +44,12 @@ app.get("/", (req, res) => {
 
 // const api_routes = require("./routes/api_routes.js");
 const routes = require("./routes");
-const { INVALID_URL } = require("./errors/errorCodes");
 app.use("/api", routes);
 
-app.get("*", (req, res, next) => {
+const { INVALID_URL } = require("./errors/errorCodes");
+app.use("*", (req, res, next) => {
   next(new error(INVALID_URL, req.originalUrl));
 });
-
-app.use(logErrorMiddleware);
-app.use(returnErrorMiddleware);
 
 app.listen(port, () => {
   logs("[INFO]", `Server listening on port ${port}`);
