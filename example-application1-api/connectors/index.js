076854cf84dcbdc0087e386823bea912f8fa64e7
seifeddine <seifeddine.benkhaled@we-settle.com>
2022-10-22 13:47:43 +0100

    connectors

--- example-application1-api/connectors/index.js
+++ example-application1-api/connectors/index.js
@@ -1,5 +1,4 @@
-const  logs  = require("../logging/log-service");
-const error = require("../errors/custom-error.model");
+const CustomError = require("../errors/custom-error.model");
 
 const connector1 = require("./connector.1");
 const connector2 = require("./connector.2");
@@ -8,7 +7,7 @@ exports.callApi = async (connector_name) => {
   var connector_function = "";
   if (connector_name === "CNX1") connector_function = connector1;
   if (connector_name === "CNX2") connector_function = connector2;
-  if (!connector_function) throw new error(INTERNAL_ERROR, "Invalid connector_function");
+  if (!connector_function) throw new CustomError(INTERNAL_ERROR, "Invalid connector "+connector_name);
   const data = await connector_function.callApi();
   return data;
 };
